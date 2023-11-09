332cf3e7114cf2eaf0a4a68fe334c121c47b0768
sthirocean <98005634+sthirocean@users.noreply.github.com>
2023-11-09 16:27:58 +0530

    Initial Commit for file: messaging_data.js

---
+++ messaging_data.js
@@ -0,0 +1,26 @@
+export const item = {
+    "conversations": [
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/413.png",
+            "id": "e524accf-9941-4c91-82cd-80d307538526",
+            "contact_name": "will",
+            "last_message": "tax",
+            "timestamp": "person"
+        },
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/717.png",
+            "id": "0a213d9d-418d-4c2f-aeba-8eec202a93b8",
+            "contact_name": "oil",
+            "last_message": "oil",
+            "timestamp": "tax"
+        },
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/697.png",
+            "id": "bd861157-5df1-4840-a014-aa69a14b098c",
+            "contact_name": "person",
+            "last_message": "oil",
+            "timestamp": "person"
+        }
+    ],
+    "id": "15dff52e-dd4d-444e-81e1-79f2c8f56778"
+}
